1448c78633859c25b5a1d1e17bce09842dad839b
DanilSPB <sherlokDP@gmail.com>
2023-02-27 18:31:50 +0300

    Added SQLite, DbContext for database. Modified the methods for dbcontext

--- Hotels.WebAPI/Program.cs
+++ Hotels.WebAPI/Program.cs
@@ -1,23 +1,69 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-var hotels = new List<Hotel>();
+builder.Services.AddDbContext<HotelDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("SQLite"));
+});
 
-app.MapGet("/hotels", () => hotels);
-app.MapGet("/hotels/{id}", (int id) => 
-    hotels.FirstOrDefault(h => h.Id == id));
-app.MapPost("/hotels", (Hotel hotel) => 
-    hotels.Add(hotel));
-app.MapPut("/hotels", (Hotel hotel) => {
-    var index = hotels.FindIndex(h => h.Id == hotel.Id);
-    if(index < 0) throw new Exception("Not found");
-    hotels[index] = hotel;
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment()) {
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<HotelDbContext>();
+    db.Database.EnsureCreated();
+}
+// GET
+app.MapGet("/hotels", async (HotelDbContext db) => await db.Hotels.ToListAsync());
+
+app.MapGet("/hotels/{id}", async (HotelDbContext db, int id) =>
+    await db.Hotels.FirstOrDefaultAsync(h => h.Id == id) is Hotel hotel ?
+        Results.Ok(hotel)
+        : Results.NotFound());
+
+// POST
+app.MapPost("/hotels", async (HotelDbContext db, [FromBody] Hotel hotel) =>
+    {
+        db.Hotels.Add(hotel);
+        await db.SaveChangesAsync();
+        return Results.Created($"/hotels/{hotel.Id}", hotel);
+    });
+
+
+// PUT
+app.MapPut("/hotels", async (HotelDbContext db, [FromBody] Hotel hotel) => {
+    var hotelFromDb = await db.Hotels.FindAsync(new object[] {hotel.Id});
+
+    if (hotelFromDb == null) return Results.NotFound();
+    
+    hotelFromDb.Name = hotel.Name;
+    hotelFromDb.Latitude = hotel.Latitude;
+    hotelFromDb.Longitude = hotel.Longitude;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 });
-app.MapDelete("/hotels/{id}", (int id) => {
-    var index = hotels.FindIndex(h => h.Id == id);
-    if(index < 0) throw new Exception("Not found");
-    hotels.RemoveAt(index);
+
+// DELETE
+app.MapDelete("/hotels/{id}", async (HotelDbContext db, int id) => {
+    var hotelFromDb = await db.Hotels.FindAsync(new object[] {id});
+
+    if (hotelFromDb == null) return Results.NotFound();
+
+    db.Hotels.Remove(hotelFromDb);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
 });
+
+app.UseHttpsRedirection();
+
 app.Run();
+
+public class HotelDbContext : DbContext {
+    public HotelDbContext(DbContextOptions<HotelDbContext> options)
+        : base(options) {}
+
+    public DbSet<Hotel> Hotels => Set<Hotel>();
+}
 
 public class Hotel {
     public int Id { get; set; }
